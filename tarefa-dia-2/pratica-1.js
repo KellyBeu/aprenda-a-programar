88be08d04310812da0a202fcebfba583191e728d
KellyBeu <kelly.beu@gmail.com>
2022-08-26 14:11:07 -0300

    Atualizacao-pratica-1

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,19 @@
+let nome
+let email
+let profisssao
+let idade 
+let destino
+
+
+nome = 'Kelly'
+email = 'kelly.beu@gmail.com'
+profisssao = 'Analista'
+idade = 42
+destino = 'Grecia'
+
+console.log(nome);
+console.log(email);
+console.log(profisssao);
+console.log(idade);
+console.log(destino);
+
