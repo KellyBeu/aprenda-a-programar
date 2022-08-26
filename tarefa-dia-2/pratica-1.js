86b163b76526d80a4737216133894326f9e0684d
KellyBeu <kelly.beu@gmail.com>
2022-08-26 16:00:17 -0300

    Inclusao_Texto

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,20 +1,23 @@
-let nome
-let email
-let profisssao
+let nome 
+let email 
+let profissao 
 let idade 
 let destino
 
 
-nome = 'Kelly'
+nome = 'Kelly Beu'
 email = 'kelly.beu@gmail.com'
-profisssao = 'Analista'
+profissao = 'Analista'
 idade = 42
 destino = 'Grecia'
 
-console.log('Nome: ' + nome,'\nE-mail: ' + email,'\nProfissão: ' + profisssao, '\nIdade: ' + idade,'\nDestino: ' + destino);
+console.log('Nome: ' + nome,'\nE-mail: ' + email,'\nProfissão: ' + profissao, '\nIdade: ' + idade,'\nDestino: ' + destino);
 
 console.log(nome);
 console.log(email);
-console.log(profisssao);
+console.log(profissao);
 console.log(idade);
 console.log(destino);
+
+
+console.log('Olá, ' + nome + '! \nSomos aqui da agência de turismo "Viagem dos Sonhos". \nEstamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para a ' + destino + '\nConfirma pra gente alguns dados? \nA sua idade é ' + idade + ' anos, você é ' + profissao + ' e o seu e-mail é ' + email + '? \nFicamos no aguardo. Muito obrigado e boa viagem! :D');
