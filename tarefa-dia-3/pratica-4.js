129483cfadb2e448cbf558ae33663007b10dba0b
KellyBeu <kelly.beu@gmail.com>
2022-08-27 22:37:08 -0300

    numero_primo

---
+++ tarefa-dia-3/pratica-4.js
@@ -0,0 +1,11 @@
+i = 2 
+numero = 1500
+
+while (numero % i != 0)
+    i++ 
+
+if (numero == i) 
+    console.log(`Sim, o número ${numero} é primo.`)
+else
+    console.log(`Não, o número ${numero} não é primo.`)
+
